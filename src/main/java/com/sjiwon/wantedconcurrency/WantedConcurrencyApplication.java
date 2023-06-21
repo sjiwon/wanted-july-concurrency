64119875ce0cd85c9a3a65a4f0a445ec9e478d46
SeoJiWon <sjiwon4491@gmail.com>
2023-06-21 15:57:58 +0900

    init commit

---
+++ src/main/java/com/sjiwon/wantedconcurrency/WantedConcurrencyApplication.java
@@ -0,0 +1,11 @@
+package com.sjiwon.wantedconcurrency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WantedConcurrencyApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(WantedConcurrencyApplication.class, args);
+	}
+}
